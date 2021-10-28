96c2dddc0a16d9e7262959998d3656edeea6f8f6
Ersin Berke Alemdaroğlu <ersinberkealemdaroglu@gmail.com>
2021-10-28 06:56:12 +0300

    Git Öğreniyorum

---
+++ src/app.js
@@ -0,0 +1,10 @@
+console.log("Merhaba Ersin")
+
+let dolarDun = 9.20
+let dolarBugun = 9.10
+if(dolarBugun < dolarDun){
+    console.log("Dolar Düştü Oku")
+}else{
+    console.log("Dolar Yükseldi Oku")
+    //console.log(dolarBugun)
+}
